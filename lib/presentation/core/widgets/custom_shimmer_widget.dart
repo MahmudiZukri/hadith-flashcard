dcb896ac160937640bc5c60b79ef5d53abb5a35f
mahmudizukri <mahmudizukri@gmail.com>
2023-08-08 16:08:36 +0700

    create custom shimmer widget

---
+++ lib/presentation/core/widgets/custom_shimmer_widget.dart
@@ -0,0 +1,39 @@
+part of 'widgets.dart';
+
+class CustomShimmerWidget extends StatelessWidget {
+  const CustomShimmerWidget({
+    required this.height,
+    required this.width,
+    required this.borderRadius,
+    super.key,
+  });
+
+  final double height;
+  final double width;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: blackColor,
+      highlightColor: whiteColor,
+      period: const Duration(
+        milliseconds: 2000,
+      ),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: blackColor.withOpacity(
+            0.04,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              borderRadius,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/core/widgets/widgets.dart
+++ lib/presentation/core/widgets/widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hadith_flashcard/domain/core/shared/shared.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:shimmer/shimmer.dart';
 
 part 'custom_text_form_field_widget.dart';
 part 'custom_elevated_button_widget.dart';
@@ -9,3 +10,4 @@ part 'custom_circular_progress_indicator_widget.dart';
 part 'custom_search_widget.dart';
 part 'custom_dropdown_button_widget.dart';
 part 'custom_app_bar_widget.dart';
+part 'custom_shimmer_widget.dart';
